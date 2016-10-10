c7e31805370c605cef5b5de0b9503cc05fe85673
TIA <minhldb@gmail.com>
2016-10-10 10:32:01 +0700

    start 1

---
+++ DemoTIA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoTIA.Startup))]
+namespace DemoTIA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
